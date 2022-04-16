b201b75d623e06da236fc665178cadc3efa60a0d
Edward Jex <edwardjex@live.co.uk>
2022-04-16 23:13:14 +0100

    Added units to graph and made fonts smaller on mobile

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -26,6 +26,8 @@ const config = {
     ]
 };
 
+const MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 window.addEventListener('load', async () => {
     const ctx = document.getElementById('riverLevelChart').getContext('2d');
     // Chart with labels every hour
@@ -36,13 +38,17 @@ window.addEventListener('load', async () => {
             datasets: []
         },
         options: {
+            maintainAspectRatio: false,
             scales: {
                 y: {
                     max: config.maxWaterLevel,
                     min: 0,
                     ticks: {
                         callback: function(value, index, values) {
-                            return value.toFixed(2);
+                            return value.toFixed(2) + 'm';
+                        },
+                        font: {
+                            size: MOBILE ? 8 : 12,
                         }
                     }
                 },
@@ -56,6 +62,11 @@ window.addEventListener('load', async () => {
                         displayFormats: {
                             hour: 'HH:mm'
                         },
+                    },
+                    ticks: {
+                        font: {
+                            size: MOBILE ? 8 : 12,
+                        }
                     }
                 }
             },
@@ -81,8 +92,20 @@ window.addEventListener('load', async () => {
                             content: limit.name,
                             enabled: true,
                             position: 'start',
+                            font: {
+                                size: MOBILE ? 8 : 12,
+                            },
                         }
                     }))
+                },
+                legend: {
+                    position: 'bottom',
+                    align: 'center',
+                    labels: {
+                        font: {
+                            size: MOBILE ? 8 : 12,
+                        }
+                    }
                 }
             }
         }
@@ -101,6 +124,8 @@ window.addEventListener('load', async () => {
             pointHitRadius: 5,
         });
         chart.update();
+        document.getElementById('latestLevel').innerText = measurements[measurements.length - 1].waterLevel.toFixed(2) + 'm';
+        document.getElementById('latestTime').innerText = moment(measurements[measurements.length - 1].timestamp).format('HH:mm');
     });
 
     getMeasurementsFromGovAPI().then((data) => {
@@ -133,7 +158,7 @@ async function getWaterLevelMeasurements() {
     const n = 1000;
     let last = Math.random() * config.maxWaterLevel;
     for (let i = 0; i < n; i++) {
-        let next = Math.max(0, Math.min(last + (Math.random() - 0.5) * 0.01, config.maxWaterLevel));
+        let next = Math.max(0, Math.min(last + (Math.random() - 0.5) * 0.001, config.maxWaterLevel));
         dummyData.push({
             timestamp: new Date(Date.now() - (i/n) * 24 * 60 * 60 * 1000),
             waterLevel: next
@@ -156,7 +181,7 @@ async function getWaterLevelMeasurements() {
 
 async function getMeasurementsFromGovAPI() {
     // Get last 24 hours of data
-    const url = `https://environment.data.gov.uk/flood-monitoring/id/stations/${config.govStationId}/readings?since=${new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString()}`;
+    const url = `https://environment.data.gov.uk/flood-monitoring/id/stations/${config.govStationId}/readings?since=${new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString()}&_sorted`;
     
     const options = {
         method: 'GET',
